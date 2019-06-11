0301338cc6d1449650d0c4d1c39e87035938fb22
Nathan Fowler <luxurysandbox@gmail.com>
2019-06-11 13:20:06 -1000

    added commenting

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const app = require('./app.js');
 const { PORT } = require('./config');
 
+// Added the port for the server to listen to via the config
+// file and app import
 app.listen(PORT, () =>{
   console.log(`Server listening on Port: ${PORT}`);
 })
